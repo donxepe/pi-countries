6d32b1e22759fe2264eede96ab2f246b932d8254
Luis Zepeda <thexepe@gmail.com>
2022-02-12 11:26:40 -0600

    countries model finished

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -8,5 +8,28 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id : {
+      type: DataTypes.STRING(3),
+      allowNull: false,
+      primaryKey: true
+    },
+    flag : {
+      type: DataTypes.STRING(),
+      allowNull: false
+    },
+    continent : {
+      type: DataTypes.STRING(30),
+      allowNull: false
+    },
+    capital : {
+      type: DataTypes.STRING(),
+      allowNull: false
+    },
+    area : {
+      type: DataTypes.INTEGER(), // even Russia's area fits in 32 bits.
+    },
+    population : {
+      type: DataTypes.INTEGER(), // even China's population fits in 32 bits.
+    },
   });
 };
