5baa13cc21ae5413b607e9365629f9089957fff9
Luis Zepeda <thexepe@gmail.com>
2022-02-16 17:09:25 -0600

    added activities action

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const GET_COUNTRIES = "GET COUNTRIES";
 export const API_GET = "http://localhost:3001/countries";
+export const GET_DETAIL = "GET DETAIL"
+export const ID_GET = "http://localhost:3001/countries"
 
 export function obtener(){
     return async function request(dispatch){
@@ -11,4 +13,14 @@ export function obtener(){
             payload: apiRequest.data
         })
     }
+}
+
+export function getActivities(countryID){
+	return async function request(dispatch){
+		let apiResponse = await axios.get(ID_GET + countryID)
+		return dispatch({
+			type: GET_DETAIL,
+			payload: apiResponse.data
+		})
+	}
 }
