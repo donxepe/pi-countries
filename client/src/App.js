10403f8fff8443c377b0f4dd8d278232917e87fb
Luis Zepeda <thexepe@gmail.com>
2022-03-01 12:45:23 -0600

    styles for header and landing

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,29 @@ import Detail from './components/detail';
 import { Route, Routes } from 'react-router-dom'
 import Activity from './components/activity';
 import Landing from './components/landing';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <h1>Podría ser una nav bar</h1>
+      <div className='App-header'>
+        <div>
+          <img className='App-logo' src="./logo512.png" alt='react logo spinning'/>
+          <span> Countries PI</span>
+        </div>
+        <div className='right-nav'>
+          <span className='header-link'>
+            <Link className='link' to={'/home'}>
+              Home
+            </Link>
+          </span>
+          <span className='header-link'>
+            <Link className='link' to={'/activity'}>
+              Create Activity
+            </Link>
+          </span>
+        </div>
+      </div>
       <Routes>
         <Route path='/home' element={<Cards />} />
         <Route path='/detail/:cID' element={<Detail />} />
--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -1,10 +1,17 @@
+import './landing.css';
 import React from 'react';
-//import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function Landing() {
 
     return(
-        <div>Ésta es una página de bienvenida</div>
-            )
+            <div className='landing-main'>
+                <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/c/c2/GDJ-World-Flags-Globe.svg' alt='globe with flags'/>
+                <h1>
+                    <Link className='landing-link' to={'/home'} >Entrar</Link>
+                    
+                </h1>
+            </div>
+        )
 
 }
